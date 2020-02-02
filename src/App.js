e3b7cf06c49a34493b007d1715097188cfaee998
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-02-02 12:38:39 -0300

    AULA03 - Manutenção estatdo componente - constructor e state

--- src/App.js
+++ src/App.js
@@ -9,17 +9,21 @@ function Welcome(props) {
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      lista: [
+        {nome:'sandro', email:'sandro@posthaus.com.br', senha:'123456'}
+      ]
+    }
+  }
+
   render() {
     return (
-      <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-    </div>
-    )
-    {/*
       <div id="layout">
+
         <a href="#menu" id="menuLink" className="menu-link">
+
           <span></span>
         </a>
 
@@ -30,20 +34,67 @@ class App extends Component {
             <ul className="pure-menu-list">
               <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
               <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livros</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+
+
             </ul>
           </div>
         </div>
 
         <div id="main">
           <div className="header">
-            <h1>Cadastro de autores</h1>
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+            <div className="pure-form pure-form-aligned">
+              <form className="pure-form pure-form-aligned">
+                <div className="pure-control-group">
+                  <label htmlFor="nome">Nome</label>
+                  <input id="nome" type="text" name="nome" value="" />
+                </div>
+                <div className="pure-control-group">
+                  <label htmlFor="email">Email</label>
+                  <input id="email" type="email" name="email" value="" />
+                </div>
+                <div className="pure-control-group">
+                  <label htmlFor="senha">Senha</label>
+                  <input id="senha" type="password" name="senha" />
+                </div>
+                <div className="pure-control-group">
+                  <label></label>
+                  <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+                </div>
+              </form>
+
+            </div>
+            <div>
+              <table className="pure-table">
+                <thead>
+                  <tr>
+                    <th>Nome</th>
+                    <th>email</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {
+                      this.state.lista.map(function(autor){
+                        return(
+                          <tr>
+                            <td>{autor.nome}</td>
+                            <td>{autor.email}</td>
+                          </tr>
+                        );
+                      })
+                    }
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
-    </div>
-    */}
-    
-  };
+
+      </div>
+    )
+  }
 }
 
 export default App;
