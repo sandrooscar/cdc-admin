7369b2db4ef409d04391ecdbc74d651f6b8e4ed0
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-02-03 21:48:17 -0300

    AULA04 - Atribuindo dados e setando this nas funcoes set

--- src/App.js
+++ src/App.js
@@ -18,9 +18,24 @@ class App extends Component {
       ],
       nome:'',
       email:'',
-      senha:''
+      senha:'',
     };
     this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value});
+  }
+
+  setEmail(evento){    
+    this.setState({email:evento.target.value});
+  }
+
+  setSenha(evento){
+    this.setState({senha:evento.target.value});
   }
 
   componentWillMount(){
@@ -92,15 +107,15 @@ class App extends Component {
               <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="Post">
                 <div className="pure-control-group">
                   <label htmlFor="nome">Nome</label>
-                  <input id="nome" type="text" name="nome" value="" />
+                  <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="email">Email</label>
-                  <input id="email" type="email" name="email" value="" />
+                  <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail}/>
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="senha">Senha</label>
-                  <input id="senha" type="password" name="senha" />
+                  <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha}/>
                 </div>
                 <div className="pure-control-group">
                   <label></label>
