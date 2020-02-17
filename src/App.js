8efcb4b6f0f6548979275e7fa81ca958c645aef7
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-02-17 20:21:33 -0300

    AULA05 - Isolano componentes de autor e melhorando comunicação entre eles - refatorando cadastro de autor

--- src/App.js
+++ src/App.js
@@ -4,73 +4,9 @@ import './css/side-menu.css';
 import $ from 'jquery';
 import InputCustomizado from './componentes/InputCustomizado';
 import ButtonCustomizado from './componentes/ButtonCustomizado';
+import { FormularioAutor, TabelaAutores } from './Autor.js';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      lista: [
-        {nome:'sandro', email:'sandro@posthaus.com.br', senha:'123456'}
-      ],
-      nome:'',
-      email:'',
-      senha:'',
-    };
-    this.enviaForm = this.enviaForm.bind(this);
-    this.setNome = this.setNome.bind(this);
-    this.setEmail = this.setEmail.bind(this);
-    this.setSenha = this.setSenha.bind(this);
-  }
-
-  setNome(evento){
-    this.setState({nome:evento.target.value});
-  }
-
-  setEmail(evento){    
-    this.setState({email:evento.target.value});
-  }
-
-  setSenha(evento){
-    this.setState({senha:evento.target.value});
-  }
-
-  componentWillMount(){
-    console.log("willMount");
-  }
-
-  componentDidMount(){
-    console.log("didMount");
-    /* faz o this apontar para o this do App, caso contrário não seria possivel manipular o setState */
-    $.ajax({
-      url:"http://localhost:8080/api/autores",
-      dataType: 'json',
-      success: function(resposta){
-        console.log("chegou a resposta");
-        this.setState({lista:resposta});
-      }.bind(this) 
-    })
-  }
-
-  //evento é um static event
-  enviaForm(evento){
-    evento.preventDefault();
-    console.log("dados enviados");
-    $.ajax({
-        url: "http://localhost:8080/api/autores",
-        contentType: 'application/json',
-        type:'post',
-        data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
-        success: function(resposta){
-          console.log(resposta);
-          console.log("enviado com sucesso");
-          this.setState({lista:resposta});
-        }.bind(this),
-        error: function(resposta){
-          console.log("erro");
-          console.log(resposta);
-        }
-    })
-  }
 
   render() {
     console.log("render");
@@ -102,35 +38,8 @@ class App extends Component {
           </div>
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="Post">
-                <InputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
-                <InputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
-                <InputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>                                                                      
-                <ButtonCustomizado type="submit" caption="Gravar"/>
-              </form>
-
-            </div>
-            <div>
-              <table className="pure-table">
-                <thead>
-                  <tr>
-                    <th>Nome</th>
-                    <th>email</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    {
-                      this.state.lista.map(function(autor){
-                        return(
-                          <tr key={autor.id}>
-                            <td>{autor.nome}</td>
-                            <td>{autor.email}</td>
-                          </tr>
-                        );
-                      })
-                    }
-                </tbody>
-              </table>
+              <FormularioAutor/>
+              <TabelaAutores/>
             </div>
           </div>
         </div>
